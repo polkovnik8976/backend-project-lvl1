9714d0d202f5a2f52a5a29f0d90dd7634ebb6e2f
Artem <Artem Petrenko>
2020-01-11 18:07:10 +0300

    refactoring calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,32 +1,33 @@
 import randomNumber from '../random_number';
-import randomSymbols from '../random_symbols';
 import run from '..';
 
 const description = 'What is the result of the expression?';
 
+const operators = ['-', '+', '*'];
 
-const answer = (firstRandomNumber, symbols, secondRandomNumber) => {
-  if (symbols === '-') {
-    return `${firstRandomNumber - secondRandomNumber}`;
+const answer = (firstRandomNumber, symbol, secondRandomNumber) => {
+  switch (symbol) {
+    case '-':
+      return `${firstRandomNumber - secondRandomNumber}`;
+    case '+':
+      return `${firstRandomNumber + secondRandomNumber}`;
+    case '*':
+      return `${firstRandomNumber * secondRandomNumber}`;
+    default:
+      return null;
   }
-  if (symbols === '+') {
-    return `${firstRandomNumber + secondRandomNumber}`;
-  }
-
-  return `${firstRandomNumber * secondRandomNumber}`;
 };
-
 
 const getRound = () => {
   const firstRandomNumber = randomNumber(1, 50);
 
   const secondRandomNumber = randomNumber(1, 50);
 
-  const symbols = randomSymbols();
+  const symbol = operators[randomNumber(0, operators.length - 1)];
 
-  const question = `${firstRandomNumber} ${symbols} ${secondRandomNumber}`;
+  const question = `${firstRandomNumber} ${symbol} ${secondRandomNumber}`;
 
-  const correctAnswer = answer(firstRandomNumber, symbols, secondRandomNumber);
+  const correctAnswer = answer(firstRandomNumber, symbol, secondRandomNumber);
 
   return { correctAnswer, question };
 };
